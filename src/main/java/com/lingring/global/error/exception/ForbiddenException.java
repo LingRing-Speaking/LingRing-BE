2af62010bca23d95395a73d624400bf788823069
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2026-04-23 01:12:13 +0900

    feat: 글로벌 예외 처리 시스템 및 작성 컨벤션 추가 (#8)
    * feat: 예외처리 시스템 구축
    
    * docs: 예외 처리 claude rule 작성

---
+++ src/main/java/com/lingring/global/error/exception/ForbiddenException.java
@@ -0,0 +1,17 @@
+package com.lingring.global.error.exception;
+
+import com.lingring.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ForbiddenException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final String message;
+
+    public ForbiddenException(final ErrorCode errorCode, final String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/lingring/global/error/exception/NotFoundException.java
@@ -0,0 +1,17 @@
+package com.lingring.global.error.exception;
+
+import com.lingring.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class NotFoundException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final String message;
+
+    public NotFoundException(final ErrorCode errorCode, final String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
