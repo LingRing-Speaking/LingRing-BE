c9ca0ae4c60782fd34a0a6cbe9a25aa6a4a951b4
CodingMasterLSW <spqjekdl1004@gmail.com>
2026-04-27 02:28:01 +0900

    chore: Redis 의존성 추가 및 Redis 테스트 환경 구축
    - Redis 의존성 추가
    - Redis TestContainers 환경 구축

--- src/test/java/com/lingring/global/config/ServiceIntegrationHelper.java
+++ src/test/java/com/lingring/global/config/ServiceIntegrationHelper.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @TestContainersTest
@@ -12,8 +13,16 @@ public abstract class ServiceIntegrationHelper {
     @Autowired
     private DataInitializer dataInitializer;
 
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
     @BeforeEach
     void setUp() {
         dataInitializer.deleteAll();
+        flushRedis();
+    }
+
+    private void flushRedis() {
+        redisConnectionFactory.getConnection().serverCommands().flushDb();
     }
 }
--- src/test/java/com/lingring/global/config/TestcontainersConfiguration.java
+++ src/test/java/com/lingring/global/config/TestcontainersConfiguration.java
@@ -1,13 +1,15 @@
 package com.lingring.global.config;
 
+import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-@EnableConfigurationProperties(DatabaseProperties.class)
+@EnableConfigurationProperties({DatabaseProperties.class, RedisContainerProperties.class})
 public class TestcontainersConfiguration {
 
     @Bean
@@ -18,5 +20,12 @@ public class TestcontainersConfiguration {
                 .withDatabaseName(databaseProperties.databaseName())
                 .withUsername(databaseProperties.username())
                 .withPassword(databaseProperties.password());
+    }
+
+    @Bean
+    @ServiceConnection
+    static RedisContainer redisContainer(final RedisContainerProperties redisProperties) {
+        return new RedisContainer(DockerImageName.parse(redisProperties.image()))
+                .withReuse(true);
     }
 }
